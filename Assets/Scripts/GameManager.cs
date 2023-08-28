65d3fcd63d775d8bec50d938072405eda91cc1b3
henry2craftman <henry2craftman@gmail.com>
2023-08-28 15:07:17 +0900

    feat: 수류탄 모델링 및 폭발 기능 추가, Option UI, GameOver 버튼 추가
    - BombAction.cs : 수류탄 폭발 기능 추가
    - GameManager.cs: Option의 각 버튼 함수 추가
    - 게임 종료시 Retry, Quit 버튼 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // 목적: 게임의 상태(Ready, Start, GameOver)를 구별하고, 게임의 시작과 끝을 TextUI로 표현하고 싶다.
 // 필요속성: 게임상태 열거형 변수, TextUI
@@ -12,7 +13,12 @@ using UnityEngine;
 // 필요속성3: hp가 들어있는 playerMove
 
 // 목적4: 플레이어의 hp가 0 이하라면, 플레이어의 애니메이션을 멈춘다.
-// 필요속성: 플레이어의 애니메이터 컴포넌트
+// 필요속성4: 플레이어의 애니메이터 컴포넌트
+
+// 목적5: Setting 버튼을 누르면 Option UI가 켜진다. 동시에 게임 속도를 조절한다(0 or 1)
+// 필요속성5: OptionUI 게임오브젝트, 일시정지 상태
+
+// 목적6: 게임 오버시 Retry와 Quit 버튼을 활성화한다.
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
@@ -22,6 +28,7 @@ public class GameManager : MonoBehaviour
     {
         Ready,
         Start,
+        Pause,
         GameOver
     }
 
@@ -31,8 +38,11 @@ public class GameManager : MonoBehaviour
     // 필요속성3: hp가 들어있는 playerMove
     PlayerMove player;
 
-    // 필요속성: 플레이어의 애니메이터 컴포넌트
+    // 필요속성4: 플레이어의 애니메이터 컴포넌트
     Animator animator;
+
+    // 필요속성5: OptionUI 게임오브젝트, 일시정지 상태
+    public GameObject optionUI;
 
     private void Awake()
     {
@@ -91,6 +101,16 @@ public class GameManager : MonoBehaviour
             // 상태 텍스트의 컬러를 빨간색으로 변경
             stateText.color = new Color32(255, 0, 0, 255);
 
+            // 목적6: 게임 오버시 Retry와 Quit 버튼을 활성화한다.
+            GameObject retryBtn = stateText.transform.GetChild(0).gameObject;
+            GameObject quitBtn = stateText.transform.GetChild(1).gameObject;
+            retryBtn.SetActive(true);
+            quitBtn.SetActive(true);
+
+            // 목적7: 게임 오버시, HP Bar와 Weapon Mode Text를 비활성화한다.
+            player.hpSlider.gameObject.SetActive(false);
+            player.GetComponent<PlayerFire>().weaponModeTxt.gameObject.SetActive(false);
+
             state = GameState.GameOver;
         }
     }
@@ -99,5 +119,48 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         CheckGameOver();
+    }
+
+    // 목적5: Option 버튼을 누르면 Option UI가 켜진다. 동시에 게임 속도를 조절한다(0 or 1)
+    // Option 화면 켜기
+    public void OpenOptionWindow()
+    {
+        // Option UI가 켜진다.
+        optionUI.SetActive(true);
+
+        // 동시에 게임 속도를 조절한다(0)
+        Time.timeScale = 0;
+
+        // 게임 상태를 변경한다.
+        state = GameState.Pause;
+    }
+
+    // 계속하기 옵션
+    public void CloseOptionWindow()
+    {
+        // Option UI가 꺼진다.
+        optionUI.SetActive(false);
+
+        // 동시에 게임 속도를 조절한다(1)
+        Time.timeScale = 1;
+
+        // 게임 상태를 변경한다.
+        state = GameState.Start;
+    }
+
+    // 다시하기 옵션
+    public void RestartGame()
+    {
+        // 동시에 게임 속도를 조절한다(1)
+        Time.timeScale = 1;
+
+        // 현재 씬 번호를 다시 로드
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    // 게임 종료 옵션
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
