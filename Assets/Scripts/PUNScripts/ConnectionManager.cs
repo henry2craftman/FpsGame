dd2675b6f40c488687e7969d49cc7233d26c0c6f
henry2craftman <henry2craftman@gmail.com>
2023-08-30 16:38:27 +0900

    feat: PUN2 세팅 완료, 접속 Scene 추가, 서버 접속 기능 추가
    - PUN2 세팅 완료
    - 접속 Scene 추가: ConnectionScene, LobbyScene, GameScene
    - ConnectionManager.cs, LobbyManager.cs 추가

---
+++ Assets/Scripts/PUNScripts/ConnectionManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// 목적: Photon 게임 서버에 연결한다.
+// 목적2: 로비에 입장한다.
+public class ConnectionManager : MonoBehaviourPunCallbacks
+{
+    public TMP_Text logText;
+    string tempTxt;
+
+    string updateTxt(out string _tempTxt, string input)
+    {
+        _tempTxt = input;
+        return tempTxt += _tempTxt;
+    }
+
+    // 목적: Photon 게임 서버에 연결한다.
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+    // 포톤 서버 연결시 호출됨
+    public override void OnConnected()
+    {
+        base.OnConnected();
+        print(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+        logText.text = updateTxt(out tempTxt, "photon server connected");
+    }
+
+    // 마스터 서버 연결시 호출됨
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        print(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+        logText.text = updateTxt(out tempTxt, "master server connected");
+
+        // 마스터 접속시 자동으로 로비 입장
+        JoinLobby();
+    }
+
+    // 목적2: 로비에 입장한다.
+    public void JoinLobby() => PhotonNetwork.JoinLobby();
+
+    // 로비에 입장시 호출됨
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        print(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+        logText.text = updateTxt(out tempTxt, "lobby enter success");
+
+        // LobbyScene 으로 이동
+        SceneManager.LoadScene("LobbyScene");
+    }
+}
---
+++ Assets/Scripts/PUNScripts/LobbyManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+
+// 목적: 로비에 방을 만들고 방에 입장한다.
+// 필요속성: 방 이름을 넣을 InputField
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    // 필요속성: 방 이름을 넣을 InputField
+    public TMP_InputField roomNameInput;
+    public int maxPlayerNum = 5;
+    public TMP_Text logText;
+    public string tempTxt;
+
+    string updateTxt(out string _tempTxt, string input)
+    {
+        _tempTxt = input;
+        return tempTxt += _tempTxt;
+    }
+
+    // 목적: 로비에 방을 만들고 방에 입장한다.
+    public void CreateRoom()
+    {
+        // inputField에 내용이 있을 때, 방을 해당 inputField의 내용으로 만든다.
+        if(roomNameInput.text != "")
+        {
+            PhotonNetwork.CreateRoom(roomNameInput.text, new RoomOptions { MaxPlayers = maxPlayerNum }, null);
+            print(System.Reflection.MethodBase.GetCurrentMethod().Name);
+        }
+    }
+
+    // 방에 입장 후 호출됨
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        print(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+        logText.text = updateTxt(out tempTxt, "enter success\n");
+    }
+
+    // 방에 개설 실패시 호출
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        base.OnCreateRoomFailed(returnCode, message);
+        print(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+        logText.text = updateTxt(out tempTxt, "create room failed\n");
+    }
+
+    // 방에 입장 실패시 호출
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        base.OnJoinRoomFailed(returnCode, message);
+        print(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+        logText.text = updateTxt(out tempTxt, "join room failed\n");
+    }
+}
