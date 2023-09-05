7d9cc30084ae2845e982c4f5a277dea7011f50ad
henry2craftman <henry2craftman@gmail.com>
2023-09-05 11:31:09 +0900

    feat: FileStream, Json serialization & deserialization
    - JSONExample.cs
    - FireStreamExample.cs

---
+++ Assets/Scripts/Utils/FileStreamExample.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.IO.Pipes;
+
+public class FileStreamExample : MonoBehaviour
+{
+    FileStream fileStream;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string path = Application.dataPath + "/fileSteam.txt";
+        fileStream = new FileStream(path, FileMode.OpenOrCreate);
+
+        //for(int i = 65; i < 90; i++)
+        //{
+        //    fileStream.WriteByte((byte)i);
+        //}
+
+        //WriteStream();
+
+        ReadStream();
+
+        fileStream.Close();
+    }
+
+    void ReadStream()
+    {
+        StreamReader streamReader = new StreamReader(fileStream);
+        string line = streamReader.ReadLine();
+
+        print(line);
+        streamReader.Close();
+    }
+
+    void WriteStream()
+    {
+        StreamWriter streamWriter = new StreamWriter(fileStream);
+        streamWriter.WriteLine("Hello Everyone");
+
+        streamWriter.Close();
+    }
+
+    void WriteCSVFormat()
+    {
+        string monsterName = "Hulk";
+        int hp = 10;
+        int speed = 50;
+        string csvFormat = string.Format("{0}, {1}, {2}", monsterName, hp, speed);
+    }
+
+    void ParseCSV()
+    {
+        string csvFormat = "Hulk, 10, 50";
+        char[] sep = { ',' };
+        string[] result = csvFormat.Split(sep);
+        
+        foreach(var s in result)
+        {
+            print(s);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/JSONExample.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Unity.VisualScripting;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class JSONExample : MonoBehaviour
+{
+    [Serializable]
+    public class Monster
+    {
+        public string name;
+        public int hp;
+        public int attackPower;
+        public int speed;
+    }
+
+    [Serializable]
+    public class StageData
+    {
+        public int stageNumber;
+        public List<Monster> monsters;
+    }
+
+    [Serializable]
+    public class TotalStageData
+    {
+        public List<StageData> stages;
+    }
+
+    public TotalStageData totalData;
+
+
+    public string StageDataReader()
+    {
+        string path = Application.dataPath + "/stages.json";
+        FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+        StreamReader streamReader = new StreamReader(fileStream);
+        string json = streamReader.ReadToEnd();
+        
+        print(json);
+        
+        streamReader.Close();
+        fileStream.Close();
+
+        return json;
+    }
+
+    void ReadJson(string json)
+    {
+        totalData = JsonUtility.FromJson<TotalStageData>(json);
+
+        foreach(var stage in totalData.stages)
+        {
+            print($"Stage number: {stage.stageNumber}");
+
+            foreach(var monster in stage.monsters)
+            {
+                print($"monster name: {monster.name}, hp: {monster.hp}, attackPower: {monster.attackPower}, speed: {monster.speed}");
+
+            }
+        }
+    }
+
+
+    [Serializable]
+    public class MyClass
+    {
+        public int level;
+        public float timeElapsed;
+        public string playerName;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Monster monster1 = new Monster();
+        monster1.name = "Hulk";
+        monster1.hp = 100;
+        monster1.attackPower = 1000;
+        monster1.speed = 50;
+
+        string json = JsonUtility.ToJson(monster1);
+
+
+        // NewtonJson 예시
+        string newtonJson = JsonConvert.SerializeObject(json);
+        JObject keyValuePairs = JObject.Parse(newtonJson);
+        JObject stageHead = (JObject)keyValuePairs["stages"];
+        if (stageHead.ContainsKey("monsters"))
+        {
+            JObject monsters = (JObject)stageHead["monsters"];
+
+            if (monsters["name"].ToString() == "Goblin")
+            {
+                monsters["hp"] = 10;
+            }
+        }
+
+        //print(json);
+
+        //WrtieJson();
+
+        string monsterJson = StageDataReader();
+        ReadJson(monsterJson);
+    }
+
+    void WrtieJson()
+    {
+        string json =
+            "{\r\n  \"level\": 11,\r\n  \"timeElapsed\": 30.55,\r\n  \"playerName\": \"New York\"\r\n}";
+
+        MyClass myClass = JsonUtility.FromJson<MyClass>(json);
+        print(myClass.level);
+    }
+}
