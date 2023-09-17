95a289e858f0cb60826a6f7ad799e6f58bb302a4
henry2craftman <141684228+henry2craftman@users.noreply.github.com>
2023-09-17 17:24:08 +0900

    chore: JsonReader, XmlReader 추가

---
+++ Assets/Scripts/JsonReader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[System.Serializable]
+public class Monster
+{
+    public string name;
+    public int health;
+}
+
+[System.Serializable]
+public class StageData
+{
+    public int stageNumber;
+    public List<Monster> monsters;
+}
+
+[System.Serializable]
+public class StageListData
+{
+    public List<StageData> stages;
+}
+
+public class JsonReader : MonoBehaviour
+{
+    void Start()
+    {
+        string path = Application.dataPath + "/stages.json"; // stages.json 파일의 경로를 지정합니다.
+        string json = File.ReadAllText(path); // stages.json 파일의 내용을 문자열로 읽어옵니다.
+
+        StageListData stageList = JsonUtility.FromJson<StageListData>(json); // JSON 문자열을 StageList 타입의 객체로 변환합니다.
+
+        foreach (var stage in stageList.stages) // 각 스테이지 정보를 출력합니다.
+        {
+            Debug.Log($"Stage Number: {stage.stageNumber}");
+            foreach (var monster in stage.monsters)
+            {
+                Debug.Log($"Monster Name: {monster.name}, Health: {monster.health}");
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/XmlReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml;
+using UnityEngine;
+
+public class XmlReader : MonoBehaviour
+{
+    void Start()
+    {
+        string path = Application.dataPath + "/stages.xml"; // stages.xml 파일의 경로를 지정합니다.
+        XmlDocument xmlDoc = new XmlDocument(); // XmlDocument 객체를 생성합니다.
+        xmlDoc.Load(path); // stages.xml 파일의 내용을 불러옵니다.
+
+        XmlNodeList stageNodes = xmlDoc.SelectNodes("/stages/stage"); // 모든 'stage' 노드를 선택합니다.
+
+        foreach (XmlNode stageNode in stageNodes) // 각 스테이지 정보를 출력합니다.
+        {
+            Debug.Log("Stage Number: " + stageNode.SelectSingleNode("stageNumber").InnerText);
+            XmlNodeList monsterNodes = stageNode.SelectNodes("monsters/monster");
+
+            foreach (XmlNode monsterNode in monsterNodes)
+            {
+                Debug.Log("Monster Name: " + monsterNode.SelectSingleNode("name").InnerText +
+                          ", Health: " + monsterNode.SelectSingleNode("health").InnerText);
+            }
+        }
+    }
+}
